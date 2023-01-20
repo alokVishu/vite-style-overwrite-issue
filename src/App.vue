897f4a225ed1e1700211c1234153b3ba49e8b433
alokVishu <alokpixinvent@gmail.com>
2023-01-20 12:20:07 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Calendar from './components/Calendar.vue'
+</script>
+
+<template>
+  <main>
+    <Calendar />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
